1b14c172a66b385d30397fae76e7c5a115689dd9
dhleong <falcone88@gmail.com>
2015-05-07 23:55:18 -0400

    Initial Commit

---
+++ SpeechTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SpeechTest
+//
+//  Created by Daniel Leong on 5/7/15.
+//  Copyright (c) 2015 Daniel Leong. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
